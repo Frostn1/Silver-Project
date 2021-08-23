7b4a4cc109bf5429512b7c35de6c10684caec8e5
Sean Dahan <aui.svi@gmail.com>
2021-08-23 22:44:36 +0300

    Structs Caption

--- lexer.py
+++ lexer.py
@@ -1,15 +1,31 @@
-import struct
+import struct as _struct
 class Lexer:
     def __init__(self, content):
         self.index = 0
         self.content = content
-        self.stru = struct.Struct()
-        self.keysDict = {'<':self.stru.detectStructs}
+        self.structs = []
         # self.data
     def lexify(self):
         for char in self.content:
-            if char in self.keysDict.keys():
-                self.index = self.keysDict[char](self.content, self.index)
+            if char == "<":
+                self.structs.append(_struct.Struct())
+                self.index = self.structs[-1].detectStructs(self.content, self.index)
                 print("current index is", self.index)
+                
             else :
                 self.index += 1
+            
+    def printStructs(self):
+        for struct in self.structs:
+            print("[Name]", struct.structName)
+            print("[Idens] =>")
+            for iden in struct.idens:
+                print("\t",iden, end="")
+                if iden in struct.callbacks.keys():
+                    print("=> [Callback]")
+                    print("\t[Func Name]", struct.callbacks[iden].name)
+                    print("\t[Func Args] =>")
+                    for arg in struct.callbacks[iden].args:
+                        print("\t\t",arg)
+                else:
+                    print()
--- struct.py
+++ struct.py
@@ -2,21 +2,35 @@
 #     first name,
 #     last name,
 #     year of birth,
-#     age => calAge(currentYear,person.year of birth),
+#     age => calAge(currentYear | person.year of birth),
 #     email
 # >
 # < nameOfStruct : idenList >
+
+class Callback:
+    def __init__(self, name : str, args : list):
+        self.name = name
+        self.args = args
+    
 class Struct:
     def __init__(self):
-        pass
+        self.callbacks = {}
+        self.idens = []
+        self.structName = ""
     def detectStructs(self, content, currentIndex):
         enderFlag = False
         for char in content[currentIndex:]:
-            if char == '>' and currentIndex > 0 and content[currentIndex-1] != '=':
+            if char == '>' and currentIndex > 0 and content[currentIndex - 1] != '=':
                 enderFlag = True
                 break
-            print(char, end="")
             currentIndex += 1
-        if not enderFlag:
-            raise Exception("lexer error : expecting `>`")
+        self.structName = content[1:content.index(":")]
+        structIden = [i.strip() for i in content[content.index(":") + 1 : currentIndex].split(",")]
+        for iden in structIden:
+            if "=>" in iden:
+                call = str(iden[iden.index("=>")+2:].strip())
+                funcName = call[:call.index("(")]
+                funcArgs = [i.strip() for i in call[call.index("(")+1:call.index(")")].split("|")]
+                self.callbacks[iden[:iden.index("=>")].strip()] = Callback(funcName, funcArgs)
+                self.idens.append(iden[:iden.index("=>")].strip())
         return currentIndex
