3669ca9c1e2937d3f21981c74170a6801835e988
Sean Dahan <aui.svi@gmail.com>
2022-01-17 15:46:30 +0200

    Modified test unit and added booleans

--- src/tools/consts.py
+++ src/tools/consts.py
@@ -4,3 +4,11 @@ EMPTY_SPACE = [' ', '\t', '\n']
 
 # Math Arithmetic
 BASIC_ARITHMETIC = ['+','-','*','/','(',')']
+
+
+# Version
+MAJOR_V = 2
+MINOR_V = 0
+PATCH_V = 0
+
+SILVER_VERSION = ".".join([str(v) for v in [MAJOR_V, MINOR_V, PATCH_V]])
